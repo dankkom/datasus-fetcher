188fa2aa7953633c1133716978aa9e324e9573d1
Daniel Komesu <danielkomesu@gmail.com>
2023-08-22 23:17:28 -0300

    Fix SIA-PA filename pattern

--- datasus_fetcher/meta.py
+++ datasus_fetcher/meta.py
@@ -58,7 +58,7 @@ uf_year2_month_pattern = r"({uf})({year})({month})".format(
     year=year_2digit_pattern,
     month=month_pattern,
 )
-uf_year2_month_pattern_sia_pa = uf_year2_month_pattern + r"([a-z])"
+uf_year2_month_pattern_sia_pa = uf_year2_month_pattern + r"(|[a-z])"
 uf_mapas_year_pattern = r"({uf})_mapas_({year})".format(
     uf=uf_pattern,
     year=year_4digit_pattern,
