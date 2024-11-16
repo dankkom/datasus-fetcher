4bbcb5bf6f834352c95bbf3d8d3b889bcc69bce0
Daniel Komesu <danielkomesu@gmail.com>
2024-11-16 09:23:29 -0300

    Feat: add docstrings, code improvements

--- datasus_fetcher/slicer.py
+++ datasus_fetcher/slicer.py
@@ -4,27 +4,26 @@ from .storage import RemoteFile
 
 
 class Slicer:
+    """
+    Slicer is a class that filters remote files based on time and regions.
+    """
+
     def __init__(
         self: Self,
-        start_time: str = None,
-        end_time: str = None,
-        regions: list[str] = None,
+        start_time: str = "",
+        end_time: str = "",
+        regions: list[str] | None = None,
     ) -> None:
-        if start_time:
-            self.start_time = start_time
-        else:
-            self.start_time = None
-        if end_time:
-            self.end_time = end_time
-        else:
-            self.end_time = None
-        if regions:
-            self.regions = regions
-        else:
-            self.regions = []
+        self.start_time = start_time
+        self.end_time = end_time
+        self.regions = regions or []
 
     def by_time(self, remote_file: RemoteFile) -> bool:
-        if not any((self.start_time, self.end_time)):
+        """
+        Filter a remote file by time.
+        """
+        # If no start or end time is provided, there is no need to filter, return True
+        if self.start_time == "" and self.end_time == "":
             return True
 
         t = ""
@@ -37,12 +36,19 @@ class Slicer:
             return t >= self.start_time
         elif not self.start_time and self.end_time:
             return t <= self.end_time
+
         return t >= self.start_time and t <= self.end_time
 
     def by_regions(self, remote_file: RemoteFile) -> bool:
-        if any(self.regions):
+        """
+        Filter a remote file by region.
+        """
+        if self.regions:
             return remote_file.partition.uf in self.regions
         return True
 
     def __call__(self, remote_file: RemoteFile) -> bool:
+        """
+        Filter a remote file by time and region.
+        """
         return self.by_regions(remote_file) and self.by_time(remote_file)
